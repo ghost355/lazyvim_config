f861dc184c77eefef47f58888f4a540477b63508
Pavel Pavlov <kx355@mail.ru>
2025-01-25 18:43:34 +0300

    add spelling for rus

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+
+-- Настраиваем авто команду для включения орфографии в определенных типах файлов
+local au = vim.api.nvim_create_autocmd
+
+au({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.txt", "*.md", "*.rst" },
+  callback = function()
+    vim.opt.spell = true
+    vim.opt.spelllang = { "ru", "en" }
+    vim.opt.spell = true
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- vim.g.noswapfile = true
+
+-- Enable Ru and EN spelling
+-- vim.opt.spelllang = { "ru", "en" }
+-- vim.opt.spell = true
