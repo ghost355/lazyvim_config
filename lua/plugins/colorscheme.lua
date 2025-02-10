7012d32a8abb314feb0f2a0287693f195031307a
P. Pavlov <kx355@mail.ru>
2025-02-10 14:26:27 +0300

    color sheme add

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,10 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
-<<<<<<< HEAD
   { "sainnhe/gruvbox-material" },
   { "maxmx03/solarized.nvim" },
-=======
   { "marko-cerovac/material.nvim" },
   { "rmehri01/onenord.nvim" },
->>>>>>> main
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
