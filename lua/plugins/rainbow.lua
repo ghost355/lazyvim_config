0c123a2da70ca6b6416c2445f66dbdcf2f92340b
Pavel Pavlov <kx355@mail.ru>
2025-02-23 19:04:55 +0300

    add new rainbow

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -1,11 +1,5 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = { "HiPhish/nvim-ts-rainbow2" },
-  opts = function(_, opts)
-    opts.rainbow = {
-      enable = true,
-      query = "rainbow-parens",
-      strategy = require("ts-rainbow").strategy.global,
-    }
-  end,
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+  },
 }
