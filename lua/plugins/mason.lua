cf55640e25a2c6494ea7402a8a81cdb3bb0197ca
Pavel Pavlov <kx355@mail.ru>
2025-03-02 02:53:33 +0300

    fix mason resourses

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,8 +1,19 @@
 return {
-  require("mason").setup({
-    registries = {
-      "github:Crashdummyy/mason-registry",
-      "github:mason-org/mason-registry",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "github:Crashdummyy/mason-registry",
+        "github:mason-org/mason-registry",
+      },
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+      max_concurrent_installers = 4,
     },
-  }),
+  },
 }
